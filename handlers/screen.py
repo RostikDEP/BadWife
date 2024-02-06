9597a4328520caf51894cc98da88e3c6a51cae33
Stadnikov Rostislav <stadnikov2000@gmail.com>
2024-02-06 19:39:43 +0200

    Created screen function

--- handlers/screen.py
+++ handlers/screen.py
@@ -1,9 +1,33 @@
-from start_bot import dp
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+from start_bot import dp, logger
+from aiogram.filters import Command
+from aiogram.types import Message, FSInputFile, BufferedInputFile
+from aiogram.enums import ChatAction
+import pyautogui
+from start_bot import bot
+import io
 
 
-@dp.message(CommandStart())
-async def command_start(message: Message):
-	await message.answer(f"{hbold}")
+
+@dp.message(Command("screen"))
+async def make_screen(message: Message):
+	try:
+		try:
+			await bot.send_message(message.chat.id, "Спроба зробити скріншот")
+			screen = pyautogui.screenshot()
+			with io.BytesIO() as buffer:
+				screen.save(buffer, format="PNG")
+				buffer.seek(0)
+				await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
+				await bot.send_photo(message.chat.id, BufferedInputFile(buffer.getvalue(), filename="screenshot.png"))
+		except Exception as e:
+			try:
+				await bot.send_message(message.chat.id, "Помилка")
+				await bot.send_message(message.chat.id, str(e))
+			except Exception as e:
+				logger.error(f"Error in make_screen>Error Notification: {str(e)}")
+
+	except Exception as e:
+		try:
+			logger.critical(f"GLOBAL ERROR im make_screen function: {str(e)}")
+		except:
+			pass
--- start_bot.py
+++ start_bot.py
@@ -3,18 +3,20 @@ from dotenv import load_dotenv
 import asyncio
 import logging
 import sys
-
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
+from loguru import logger
+from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv("TEST_TOKEN")
 
 dp = Dispatcher()
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
 
+logger.add(f"logs/{datetime.now().strftime("%Y-%m-%d %H-%M-%S")}.log", format="{time} {level} {message}", rotation="10 MB", compression="zip")
 
 async def main():
-	bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
 	await dp.start_polling(bot)
 
 
