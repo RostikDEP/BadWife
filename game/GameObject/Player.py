b2543d5109b406eccd38235695ee04fe328cfb75
Stadnikov Rostislav <stadnikov2000@gmail.com>
2024-02-15 18:40:47 +0200

    Created character animation

--- game/GameObject/Player.py
+++ game/GameObject/Player.py
@@ -13,7 +13,7 @@ class Player:
 		self.bottom_spase = 100
 		self.direction = True
 		self.animate_step = 0
-		self.animate_speed = 5
+		self.animate_speed = 3
 		self.animate_step_limit = 8 * self.animate_speed
 		self.right_position = (config.SIZE[0] - config.bg_tile_size * 2 - self.size[0], config.SIZE[1] - self.bottom_spase)
 		self.left_position = (config.bg_tile_size * 2, config.SIZE[1] - self.bottom_spase)
@@ -30,11 +30,13 @@ class Player:
 	def Update(self, events):
 		self.events = events
 		self.UpdatePosition()
+		self.Animate()
 		self.Render()
+		
 
 
 	def Render(self):
-		self.surface.blit(self.src_images[0], (0, 0))
+		self.surface.blit(self.image, (0, 0))
 		self.surface = pygame.transform.flip(self.surface, not self.direction, 0)
 		self.screen.blit(self.surface, self.position)
 
@@ -49,3 +51,13 @@ class Player:
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_SPACE:
 					self.direction = not self.direction
+
+
+	def Animate(self):
+		if self.animate_step >= self.animate_step_limit:
+			self.animate_step = 0
+		else:
+			self.animate_step += 1
+
+		self.surface.fill("white")
+		self.image = self.src_images[self.animate_step // 8]
